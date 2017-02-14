4803c4b8d6e828e096b00a6af1efce43f1d17f72
Chris Mimms <Chris Mimms>
2017-02-13 22:56:24 -0500

    Merge head and update for background color.

--- docs/nose-pushup-counter/nose.js
+++ docs/nose-pushup-counter/nose.js
@@ -1,11 +1,21 @@
+var timer = setTimeout(updatePage, 10);
+var bgColor = 255;
+
 function updatePage(){
     //This does nothing yet
+    var oldColor = document.body.style.backgroundColor;
+    bgColor -= 1;
+    document.body.style.backgroundColor = `rgb(${bgColor},${bgColor},${bgColor})`;
+    var timer = setTimeout(updatePage, 5);
 }
 
 function nosePress(){
     //What happens when you touch the screen
+    clearTimeout(timer);
     var counter = document.getElementById("counter");
     var num = parseInt(counter.innerHTML);
     num +=1;
     counter.innerHTML = num;
+    document.body.style.backgroundColor = "#ffffff";
+    bgColor = 255;
 }
